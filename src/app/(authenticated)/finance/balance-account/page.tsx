0c0459b05708363f58071ce09ac604ed8d355276
“cielhaidir” <phoenicchi@gmail.com>
2025-08-27 21:31:05 +0800

    fix: update required permissions for ProtectedRoute in balance account page

--- src/app/(authenticated)/finance/balance-account/page.tsx
+++ src/app/(authenticated)/finance/balance-account/page.tsx
@@ -85,7 +85,7 @@ export default function BalanceAccountPage() {
   });
 
   return (
-    <ProtectedRoute requiredPermissions={["menu:finance-balance-account"]}>
+    <ProtectedRoute requiredPermissions={["menu:balances"]}>
       <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
